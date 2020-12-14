959ab4ab051ee0a7abc040c1f36745a25ba9b9a4
dimas1997 <dimas6597@gmail.com>
2020-12-14 08:49:32 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# PyAugmentation  Video
+
+A python script for augmentation data video 
+
---
+++ handler_video.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+def video2frame(file_video):
+    cap= cv2.VideoCapture(file_video)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    frame_arr = []
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        
+        frame_arr.append(frame)
+    
+    return frame_arr,fps
+
+def frame2video(frame_array,fps,pathOut):
+    height, width, layers = frame_array[0].shape
+    size = (width,height)
+
+    out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
+
+    for i in range(len(frame_array)):
+        # writing to a image array
+        out.write(frame_array[i])
+    out.release()
